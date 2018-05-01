66ad08618a8ffee15a814aad0322428d06711ce7
skichrome <toni.campeol@outlook.fr>
2018-05-01 16:27:30 +0200

    Added Map view support with google api. [WARNING] If you want to test the app you must add your own api key with your own SHA-1 debug footprint Now when we launch the app we have a map with a pin on Lyon.
    Signed-off-by: skichrome <toni.campeol@outlook.fr>

--- app/src/main/java/com/skichrome/go4lunch/controllers/fragments/MapFragment.java
+++ app/src/main/java/com/skichrome/go4lunch/controllers/fragments/MapFragment.java
@@ -1,18 +1,29 @@
 package com.skichrome.go4lunch.controllers.fragments;
 
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.skichrome.go4lunch.R;
 import com.skichrome.go4lunch.base.BaseFragment;
+
+import static android.support.constraint.Constraints.TAG;
 
 /**
  * This Fragment is used to display a mapView with map api, it will display some restaurants around the user mainly
  */
-public class MapFragment extends BaseFragment
+public class MapFragment extends BaseFragment implements OnMapReadyCallback
 {
     //=========================================
     // Fields
     //=========================================
+
+    private GoogleMap gMap;
 
     //=========================================
     // New Instance method
@@ -50,5 +61,22 @@ public class MapFragment extends BaseFragment
 
     private void configureMapApi()
     {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragment_map_map_view);
+        mapFragment.getMapAsync(this);
+    }
+
+    //=========================================
+    // GMap Methods
+    //=========================================
+
+    @Override
+    public void onMapReady(GoogleMap mGoogleMap)
+    {
+        Log.d(TAG, "onMapReady: _______________________________________");
+        this.gMap = mGoogleMap;
+
+        LatLng lyon = new LatLng(45.75, 4.85);
+        gMap.addMarker(new MarkerOptions().position(lyon).title("Marker in lyon"));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(lyon));
     }
 }
